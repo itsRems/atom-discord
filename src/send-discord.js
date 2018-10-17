e5afe7bbc3075fbc9c600428f6de7b03fa63ce11
Nicolas Theck <33380002+itsRems@users.noreply.github.com>
2018-10-17 19:39:17 +0200

    aaaaaaaa

--- src/send-discord.js
+++ src/send-discord.js
@@ -3,8 +3,6 @@ const path = require('path');
 const {Client} = require('../node_modules/discord-rpc/');
 const matched = require('../data/matched.json');
 
-const DISCORD_ID = '422582319459598337';
-
 if (!String.prototype.padStart) {
 	String.prototype.padStart = function padStart(targetLength,padString) {
 		targetLength = targetLength >> 0; //floor if number or convert non-number to 0;
@@ -45,7 +43,11 @@ const config = {
 	}))
 };
 
-if(!config.behaviour.appId) DISCORD_ID = '422582319459598337';
+if(!config.behaviour.appId){
+	const DISCORD_ID = '422582319459598337';
+}else {
+	const DISCORD_ID = config.behaviour.appId;
+}
 
 const normalize = (object) => {
 	Object.keys(object).forEach((k) => {
